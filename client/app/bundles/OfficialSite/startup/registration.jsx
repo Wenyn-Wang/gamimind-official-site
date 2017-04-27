fa0bacfce202d00e3c3290c0d7f9519910323ed1
roland31314 <click21314@gmail.com>
2017-04-27 15:57:50 +0800

    hot loader

--- client/app/bundles/OfficialSite/startup/registration.jsx
+++ client/app/bundles/OfficialSite/startup/registration.jsx
@@ -1,8 +1,27 @@
-import ReactOnRails from 'react-on-rails';
+import React from "react"
+import { AppContainer } from "react-hot-loader"
+import { render } from "react-dom"
+import ReactOnRails from "react-on-rails"
+import Home from "../components/Home"
 
-import Home from '../components/Home';
+const consoleErrorReporter = ({ error }) => {
+  console.error(error) // eslint-disable-line
+  return null
+}
+consoleErrorReporter.propTypes = {
+  error: React.PropTypes.instanceOf(Error).isRequired
+}
 
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  Home,
-});
+const HomeHotLoader = (props, railsContext, domNodeId) => {
+  const renderApp = (Component) => {
+    const element = (
+      <AppContainer errorReporter={consoleErrorReporter}>
+        <Component />
+      </AppContainer>
+    )
+    render(element, document.getElementById(domNodeId))
+  }
+  renderApp(Home)
+}
+
+ReactOnRails.register({ Home: HomeHotLoader })
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,27 +6,51 @@ const webpack = require('webpack');
 const pathLib = require('path');
 
 const devBuild = process.env.NODE_ENV !== 'production';
+const options = {
+  entry: devBuild ? [
+    "webpack-dev-server/client?http://localhost:3500",
+    "react-hot-loader/patch",
+  ] : [],
+  devtool : devBuild ? 'eval-source-map' : false,
+  devServer : devBuild ? {
+    port: 3500,
+    hot: true,
+    stats: {
+      hash: false,
+      version: false,
+      chunks: false
+    }
+  } : false,
+  plugins : devBuild ? [  new webpack.HotModuleReplacementPlugin() ] : [],
+}
 
 const config = {
   entry: [
     'es5-shim/es5-shim',
     'es5-shim/es5-sham',
     'babel-polyfill',
+    ...options.entry,
     './app/bundles/OfficialSite/startup/registration',
     './app/assets/styles/OfficialSite/index.scss',
   ],
 
   output: {
     filename: 'webpack-bundle.js',
-    path: pathLib.resolve(__dirname, '../app/assets/webpack'),
+    path: pathLib.resolve(__dirname, '../app/assets/javascripts/webpack/'),
   },
 
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+
+  devtool : options.devtool,
+  devServer : options.devServer,
+
   plugins: [
     new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
+    ...options.plugins,
   ],
+  
   module: {
     rules: [
       {
@@ -64,7 +88,6 @@ module.exports = config;
 
 if (devBuild) {
   console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-  module.exports.devtool = 'eval-source-map';
 } else {
   console.log('Webpack production build for Rails'); // eslint-disable-line no-console
 }
