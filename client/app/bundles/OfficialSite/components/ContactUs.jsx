b7102af6de1b174cb571b2f884c635c4272b3b1f
roland31314 <click21314@gmail.com>
2017-04-28 10:44:56 +0800

    建個jsxu先

---
+++ client/app/bundles/OfficialSite/components/ContactUs.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+ class ContactUs extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>我們會儘速與您聯絡</h1>
+      </section>
+    );
+  }
+}
+
+ContactUs.propTypes = {
+}
+
+export default ContactUs
--- client/app/bundles/OfficialSite/components/Home.jsx
+++ client/app/bundles/OfficialSite/components/Home.jsx
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import ContactUs from './ContactUs'
 
 export default class Home extends React.Component {
   static propTypes = {
@@ -7,11 +8,9 @@ export default class Home extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1>遊戲話社群媒體方案</h1>
-        <h4>百萬粉絲醒過來，不要再封鎖我了！</h4>
-        <h4>把百萬粉絲，變成真正的"品牌用戶"</h4>
-      </div>
+      <main>
+        <ContactUs />
+      </main>
     );
   }
 }
