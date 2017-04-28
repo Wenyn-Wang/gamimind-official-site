e76499aacb86b08250d88f511b2bd5ea0247c2bc
roland31314 <click21314@gmail.com>
2017-04-28 13:02:50 +0800

    第一頁的JSX

--- client/app/bundles/OfficialSite/components/ContactUs.jsx
+++ client/app/bundles/OfficialSite/components/ContactUs.jsx
@@ -4,8 +4,16 @@ import PropTypes from 'prop-types';
  class ContactUs extends React.Component {
   render() {
     return (
-      <section>
-        <h1>我們會儘速與您聯絡</h1>
+      <section id="contact-us">
+        <div className="contact-us-center">
+          <h1>和我們一起精準預測！</h1>
+          <div className="company-info">
+            <p>電話：+886-2-23681580</p>
+            <p>email：info@gamemind.org</p>
+            <p>地址：台北市中山區羅斯福路三段302號12樓之1</p>
+          </div>
+          <div className="btn primary">合作洽談</div>
+        </div>
       </section>
     );
   }
