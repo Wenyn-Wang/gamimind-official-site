7dcf40d022db6e2de9c27c1b8eb5b9678ac29cac
roland31314 <click21314@gmail.com>
2017-05-03 17:38:05 +0800

    先建個 cover jsx & scss

--- client/app/bundles/officialSite/components/home/Cover.jsx
+++ client/app/bundles/officialSite/components/home/Cover.jsx
@@ -1,15 +1,13 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import Cover from './Cover'
-import ContactUs from './ContactUs'
 
 export default class Home extends React.Component {
   render() {
     return (
-      <main>
-        <Cover />
-        <ContactUs />
-      </main>
+      <section id="cover">
+        cover
+
+      </section>
     )
   }
 }
