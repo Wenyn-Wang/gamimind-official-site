625cad6e1d0930d3b60149052166f508fcaa1bb4
roland31314 <click21314@gmail.com>
2017-05-04 16:04:24 +0800

    換符號

--- client/app/bundles/officialSite/components/home/Cover.jsx
+++ client/app/bundles/officialSite/components/home/Cover.jsx
@@ -8,7 +8,7 @@ export default class Home extends React.Component {
         <div className="cover-left">
           <div className="center">
             <h1>人工智慧，為你解析數據下的商機</h1>
-            <h2>我們相信 “企業做到精準才有未來” <br />融入遊戲思維，成為有溫度的人工智慧。<br />精準掌握客戶行為與喜好，為你找出數據下的商機！</h2>
+            <h2>我們相信「企業做到精準才有未來」 <br />融入遊戲思維，成為有溫度的人工智慧。<br />精準掌握客戶行為與喜好，為你找出數據下的商機！</h2>
             <div className="btn primary">我們的服務</div>
           </div>
         </div>
