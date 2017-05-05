9607b5f036208b8698f5b877856c4b2a26f518ce
roland31314 <click21314@gmail.com>
2017-05-05 10:43:51 +0800

    pc layout

--- client/app/bundles/officialSite/components/home/Problems.jsx
+++ client/app/bundles/officialSite/components/home/Problems.jsx
@@ -40,20 +40,20 @@ const Problems = () => {
           </div>
         </div>
         
-        <div className="problems-container">
-          <div className="problem">
+        <ul className="problems">
+          <li className="problem">
             <img src={ problem_img1 } />
             <p>總是抓不住用戶的心，花大錢買廣告卻看不到業績起飛？</p>
-          </div>
-          <div className="problem">
+          </li>
+          <li className="problem">
             <img src={ problem_img2 } />
             <p>經營 Facebook/Line 社群許久，卻苦於無法將粉絲轉為忠實顧客？</p>
-          </div>
-          <div className="problem">
+          </li>
+          <li className="problem">
             <img src={ problem_img3 } />
             <p>有百萬粉絲，卻全是殭屍，想成為客戶肚子裡的蛔蟲，卻永遠只能當旁邊的蒼蠅？</p>
-          </div>
-        </div>
+          </li>
+        </ul>
       </div>
     </section>
   )
