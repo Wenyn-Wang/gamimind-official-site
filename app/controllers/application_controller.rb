a634d38c952bd6f259b2266790dcec21e4bb84c1
mia81514 <mia81514@gmail.com>
2017-05-09 15:51:39 +0800

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ok(data = {})
+    render json: data, status: 200
+  end
+
+  def error(msg = "")
+    render json: {error: msg}, status: 500
+  end
 end
