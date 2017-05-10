dd8077981b00925e4d04f1d37a8c9038a9d08e3c
roland31314 <click21314@gmail.com>
2017-05-10 16:56:48 +0800

    footer

---
+++ client/app/bundles/officialSite/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© Copyright 2017 Gamemind. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
