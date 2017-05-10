3bad8847891dc35b226a4ceee2d254351b399b2c
mia81514 <mia81514@gmail.com>
2017-05-10 17:07:32 +0800

    CompanyInfo獨立

--- client/app/bundles/officialSite/components/contactUs/ContactUs.jsx
+++ client/app/bundles/officialSite/components/contactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ContactForm from './ContactForm'
+import CompanyInfo from './CompanyInfo'
 
 const steps = ["welcome", "form", "success"]
 
@@ -30,19 +31,20 @@ export default class ContactUs extends Component {
             step == steps[0] &&
               <div className="company-info-container">
                 <h1>和我們一起精準預測！</h1>
-
-                <div className="company-info">
-                  <p>電話：+886-2-23681580</p>
-                  <p>email：info@gamemind.org</p>
-                  <p>地址：台北市中正區羅斯福路三段302號12樓之1</p>
-                </div>
-
+                <CompanyInfo />
                 <div className="btn primary" onClick={ this.goForm }>合作洽談</div>
               </div>
           }
           {
             step == steps[1] &&
               <ContactForm goBack={ this.goWelcome } />
+          }
+          {
+            step == steps[2] &&
+              <div className="company-info-container">
+                <h1>我們會儘速與您聯絡！</h1>
+                <CompanyInfo />
+              </div>
           }
         </div>
       </section>
