b6714f3423a3587c0306da2735b7320553675d2c
mia81514 <mia81514@gmail.com>
2017-05-10 18:03:08 +0800

    這樣比較好

--- client/app/bundles/officialSite/components/common/Footer.jsx
+++ client/app/bundles/officialSite/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <p>© Copyright 2017 Gamemind. All rights reserved.</p>
+      <p>© Copyright 2017 GameMind Inc. All rights reserved.</p>
     </footer>
   )
 }
