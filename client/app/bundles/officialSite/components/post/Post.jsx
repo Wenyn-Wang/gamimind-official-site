203dbc3439c66f75931d4cabbc2c2ff35aa74c28
mia81514 <mia81514@gmail.com>
2017-05-11 11:34:38 +0800

    路徑串串～～

--- client/app/bundles/officialSite/components/post/Post.jsx
+++ client/app/bundles/officialSite/components/post/Post.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
+import { getPost } from '../../apis/postApis'
 
 export default class Post extends Component {
+  constructor(props) {
+    super(props)
+    const { slug } = props.match.params
+    this.state = {
+      slug,
+      title   : "",
+      content : "",
+      img     : "",
+    }
+    getPost({ slug }).then(data => {
+      this.setState({ ...data })
+    })
+  }
   render() {
     return (
       <main id="post">
--- client/app/bundles/officialSite/constants/url.js
+++ client/app/bundles/officialSite/constants/url.js
@@ -1,3 +1,4 @@
 export const HOME = '/'
 export const SERVICE = '/service'
 export const BLOG = '/blog'
+export const POST = '/post'
