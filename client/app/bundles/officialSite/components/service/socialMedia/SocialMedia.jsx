8884dc11d78c4395cc3a790379e84393feb464bf
roland31314 <click21314@gmail.com>
2017-05-15 12:19:07 +0800

    創個jsx scss

--- client/app/bundles/officialSite/components/service/socialMedia/SocialMedia.jsx
+++ client/app/bundles/officialSite/components/service/socialMedia/SocialMedia.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cover from './Cover'
+import Trouble from './Trouble'
 
 const SocialMedia = () => {
   return (
     <main id="social-media">
       <Cover />
+      <Trouble />
     </main>
   )
 }
---
+++ client/app/bundles/officialSite/components/service/socialMedia/Trouble.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Trouble = () => {
+  return (
+    <section id="trouble">
+      困擾
+    </section>
+  )
+}
+
+export default Trouble
