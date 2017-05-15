9df7a91b0a5c11ca6c6e0623332a5e1a0da59871
roland31314 <click21314@gmail.com>
2017-05-15 16:49:41 +0800

    先建個jsx scss

---
+++ client/app/bundles/officialSite/components/service/socialMedia/Solutions.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Solutions = () => {
+  return (
+    <section id="solutions">
+      <div className="solutions-title">
+        <h1>我們的方法</h1>
+      </div>
+      
+      
+    </section>
+  )
+}
+
+export default Solutions
