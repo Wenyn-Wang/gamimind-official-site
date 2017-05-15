07eee60c4620c8d970a952f22b8840d1f2c2164b
roland31314 <click21314@gmail.com>
2017-05-15 12:05:10 +0800

    建jsx 跟 scss

---
+++ client/app/bundles/officialSite/components/service/socialMedia/SocialMedia.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cover from './Cover'
+
+const SocialMedia = () => {
+  return (
+    <main id="social-media">
+      <Cover />
+    </main>
+  )
+}
+
+export default SocialMedia
