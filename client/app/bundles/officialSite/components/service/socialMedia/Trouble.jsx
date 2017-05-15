217eada93cae5d288deae12eda541143def97a69
roland31314 <click21314@gmail.com>
2017-05-15 13:51:38 +0800

    頁面上的字

--- client/app/bundles/officialSite/components/service/socialMedia/Trouble.jsx
+++ client/app/bundles/officialSite/components/service/socialMedia/Trouble.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Trouble = () => {
   return (
     <section id="trouble">
-      困擾
+      <h2 className="trouble-title">困擾</h2>
+      <p>你了解你的社群用戶嗎？</p>
+      <p>超過100萬人氣追蹤，按讚 Facebook 與 Line，但實際的忠實用戶有多少呢？</p>
+      <p>企業推出的貼圖超可愛，用戶加入官方帳號、下載貼圖後，如何避免被“封鎖”？</p>
     </section>
   )
 }
