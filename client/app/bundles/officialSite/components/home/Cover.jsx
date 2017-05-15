31afe08efe4d1970b386f4d1548905fedf386496
roland31314 <click21314@gmail.com>
2017-05-15 17:18:42 +0800

    補個頁內連結

--- client/app/bundles/officialSite/components/home/Cover.jsx
+++ client/app/bundles/officialSite/components/home/Cover.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { HASH } from '../../constants/url'
+import { Link as ScrollLink } from 'react-scroll'
 
 export default class Cover extends Component {
   render() {
@@ -8,7 +10,14 @@ export default class Cover extends Component {
           <div className="center">
             <h1>人工智慧，為你解析數據下的商機</h1>
             <h2>我們相信「企業做到精準才有未來」 <br />融入遊戲思維，成為有溫度的人工智慧。<br />精準掌握客戶行為與喜好，為你找出數據下的商機！</h2>
-            <div className="btn primary">我們的服務</div>
+            <ScrollLink
+              smooth
+              to        = { HASH.SERVICES } 
+              duration  = { 500 }
+              className = "btn primary"
+            >
+              我們的服務
+            </ScrollLink>
           </div>
         </div>
 
