2ab6ce6da717c32d1ba8fd34009e61d72cb00a46
roland31314 <click21314@gmail.com>
2017-05-15 15:01:30 +0800

    文字 ＆ 背景圖 style

--- client/app/bundles/officialSite/components/service/socialMedia/Trouble.jsx
+++ client/app/bundles/officialSite/components/service/socialMedia/Trouble.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Trouble = () => {
   return (
     <section id="trouble">
-      <div className="trouble-title">
-        <h2>困擾</h2>
+      <div className="section-title">
+        <h1>困擾</h1>
       </div>
       
-      <p>你了解你的社群用戶嗎？</p>
-      <p>超過100萬人氣追蹤，按讚 Facebook 與 Line，但實際的忠實用戶有多少呢？</p>
-      <p>企業推出的貼圖超可愛，用戶加入官方帳號、下載貼圖後，如何避免被“封鎖”？</p>
+      <div className="trouble-text">
+        <p>你了解你的社群用戶嗎？</p>
+        <p>超過100萬人氣追蹤，按讚 Facebook 與 Line，但實際的忠實用戶有多少呢？</p>
+        <p>企業推出的貼圖超可愛，用戶加入官方帳號、下載貼圖後，如何避免被“封鎖”？</p>
+      </div>
     </section>
   )
 }
