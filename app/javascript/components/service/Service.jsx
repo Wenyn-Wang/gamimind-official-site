8f4be9fe318bfedeb386687bdd4e07384aa68292
mia81514 <mia81514@gmail.com>
2017-05-16 22:21:44 +0800

    cp original files

---
+++ app/javascript/components/service/Service.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Service extends Component {
+  render() {
+    return (
+      <main>
+        服務頁
+      </main>
+    )
+  }
+}
