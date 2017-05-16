ea0d72c855f8961422c9d638e73ccb0635d04fc2
roland31314 <click21314@gmail.com>
2017-05-16 14:39:03 +0800

    pc layout

--- client/app/bundles/officialSite/components/service/socialMedia/Solutions.jsx
+++ client/app/bundles/officialSite/components/service/socialMedia/Solutions.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const solutions = [{
   title : '評估',
   text  : '企業形象、品牌定位、市場動態',
-  img   : '',
+  img   : require('images/service/socialMedia/solutions/step1.png'),
 }, {
   title : '遊戲化導入',
   text  : '設計遊戲化文案，深化使用者互動',
-  img   : '',
+  img   : require('images/service/socialMedia/solutions/step2.png'),
 }, {
   title : '工具開發',
   text  : '建置 Line Add-on 遊戲化工具，Facebook 小遊戲',
-  img   : '',
+  img   : require('images/service/socialMedia/solutions/step3.png'),
 }, {
   title : '營運',
   text  : '結合「企業自媒體平台」整合性營運，分析用戶需求、標定客戶樣貌、發送客製化推薦',
-  img   : '',
+  img   : require('images/service/socialMedia/solutions/step4.png'),
 }]
 
 const Solutions = () => {
@@ -33,7 +33,7 @@ const Solutions = () => {
                 <h2>{ solution.title }</h2>
               </div>
 
-              <p>{ solution.text }</p>
+              <p className="solution-text">{ solution.text }</p>
 
               <div className="solution-img">
                 <img src={ solution.img } />
