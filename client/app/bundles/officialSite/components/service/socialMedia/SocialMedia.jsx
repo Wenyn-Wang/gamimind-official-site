a99e73788b7e2042de4b3a12a630f71e83204cba
roland31314 <click21314@gmail.com>
2017-05-17 10:46:21 +0800

    html結構

--- client/app/bundles/officialSite/components/service/socialMedia/SocialMedia.jsx
+++ client/app/bundles/officialSite/components/service/socialMedia/SocialMedia.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cover from './Cover'
 import Trouble from './Trouble'
+import ExpectedResult from './ExpectedResult'
 
 const SocialMedia = () => {
   return (
     <main id="social-media">
       <Cover />
       <Trouble />
+      <ExpectedResult />
     </main>
   )
 }
