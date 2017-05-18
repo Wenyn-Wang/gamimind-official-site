9d49e51b08122c885fcd26b2b58861c89cdd4136
roland31314 <click21314@gmail.com>
2017-05-18 17:22:10 +0800

    印字先

--- app/javascript/components/service/socialMedia/SocialMedia.jsx
+++ app/javascript/components/service/socialMedia/SocialMedia.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cover from './Cover'
 import Trouble from './Trouble'
+import WhatKindOfPerson from './WhatKindOfPerson'
 
 const SocialMedia = () => {
   return (
     <main id="social-media">
       <Cover />
       <Trouble />
+      <WhatKindOfPerson />
     </main>
   )
 }
