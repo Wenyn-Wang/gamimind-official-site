54e69354692b7aaf79dc9a3690d4352c2f629a4a
mia81514 <mia81514@gmail.com>
2017-05-19 16:30:03 +0800

    CustomerRelationship - Cover

--- app/javascript/components/service/customerRelationship/CustomerRelationship.jsx
+++ app/javascript/components/service/customerRelationship/CustomerRelationship.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Cover from './Cover'
 
 const CustomerRelationShip = () => {
   return (
     <main id="customer-relationship">
+      <Cover />
     </main>
   )
 }
