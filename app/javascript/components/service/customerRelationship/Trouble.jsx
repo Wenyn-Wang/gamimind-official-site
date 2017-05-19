f3a6fe0f5ed659df45248387ecccbeddf462fb38
mia81514 <mia81514@gmail.com>
2017-05-19 16:39:15 +0800

    Trouble

---
+++ app/javascript/components/service/customerRelationship/Trouble.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Trouble = () => {
+  return (
+    <section id="trouble">
+      <div className="section-title">
+        <h1>困擾</h1>
+      </div>
+      
+      <div className="trouble-text">
+        <p>現在用line經營不方便，滑開手機看到100個未讀好緊張！</p>
+        <p>不夠了解客戶，在錯的時間出現，總是吃閉門羹！</p>
+        <p>客戶不會主動表達需求，花了大把時間，仍雞同鴨講！</p>
+      </div>
+    </section>
+  )
+}
+
+export default Trouble
