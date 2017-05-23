4037469c6b73a053a71a4664789f0046a675f9ac
roland31314 <click21314@gmail.com>
2017-05-23 10:43:53 +0800

    困難

--- app/javascript/components/service/weMedia/WeMedia.jsx
+++ app/javascript/components/service/weMedia/WeMedia.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Cover from '../common/Cover'
+import Trouble from '../common/Trouble'
 import Solutions from '../common/Solutions'
 
-const cover_attr = {
-  title : '品牌自媒體經營方案',
-  desc  : '強力磁鐵把粉絲都吸過來！讓我們幫你輕鬆打造品牌生態圈，成為業界意見領袖！',
-}
+const trouble_text = (
+  <div>
+    <p>用品牌整合資源，跨領域發展，真正發揮出品牌的力量</p>
+    <p>打造生態圈即要求企業用品牌的背書與積累起來的信任，在自己的主營業務外</p>
+    <p>實現跨界整合，為消費者創造更多的價值，提供一線話的服務</p>
+    <p>同時，況大企業的未來與版圖</p>
+  </div>
+)
 
 const solution_attr = {
   steps : [{
@@ -46,7 +51,14 @@ const solution_attr = {
 const WeMedia = () => {
   return (
     <main id="social-media">
-      <Cover { ...cover_attr } />
+      <Cover 
+        title = "品牌自媒體經營方案"
+        desc  = "強力磁鐵把粉絲都吸過來！讓我們幫你輕鬆打造品牌生態圈，成為業界意見領袖！"
+      />
+      <Trouble
+        title = "在這個快速變化的時代，品牌需的要是打造生態圈"
+        text  = { trouble_text }
+      />
       <Solutions { ...solution_attr } />
     </main>
   )
